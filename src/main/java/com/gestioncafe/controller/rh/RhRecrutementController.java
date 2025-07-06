db2c14579c751f919f53ab0a9f7696a22af2ea68
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2025-07-06 21:59:39 +0300

    transaction recrutement

--- src/main/java/com/gestioncafe/controller/rh/RhRecrutementController.java
+++ src/main/java/com/gestioncafe/controller/rh/RhRecrutementController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpServletResponse;
 
